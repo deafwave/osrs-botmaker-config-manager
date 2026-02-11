9da0d4c064b24f86634016760988f57f2a92a338
Khoguu <dweebstify@gmail.com>
2026-02-10 18:45:54 -0600

    chore: clean-up

--- src/index.npm.ts
+++ src/index.npm.ts
@@ -2,9 +2,6 @@ import { createConfigScope, logConfigValues } from './data/config-scope.js'
 import { sendConfig } from './data/group/keys.js'
 import { group, profile } from './index.js'
 
-export type { ConfigScope, ConfigScopeMode, ConfigScopeOptions, LogConfigValuesOptions } from './data/config-scope.js'
-export type { ConfigWriteOptions } from './data/shared/types.js'
-
 export const config = {
 	group,
 	profile,
@@ -12,7 +9,3 @@ export const config = {
 	logConfigValues,
 	sendConfig,
 }
-
-export { group, profile } from './index.js'
-export { createConfigScope, logConfigValues } from './data/config-scope.js'
-export { sendConfig } from './data/group/keys.js'
